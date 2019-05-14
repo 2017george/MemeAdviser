d88896389e436d3e5106e2c0b80f258f2b797232
Reece Dunham <me@rdil.rocks>
2019-05-14 19:31:22 -0400

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,1 +1,13 @@
+import os
+import app
 
+test = os.getenv("test") not None
+travis = os.getenv("HAS_JOSH_K_SEAL_OF_APPROVAL") not None
+
+if travis:
+	# travus sucks but whatever
+	os.system("python -m pytest ./tests -v --cov=./src")
+elif test and not travis:
+	os.system("python3 -m pytest ./tests -v")
+else:
+	app.main()
